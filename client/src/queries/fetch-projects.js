e2f57255c147dd792f078b7cd792de67971ba4f1
smitsleon <leon.smits@student.arteveldehs.be>
2025-06-05 09:12:14 +0200

    Add Prettier for code formatting and update various files for consistency

--- client/src/queries/fetch-projects.js
+++ client/src/queries/fetch-projects.js
@@ -1,11 +1,11 @@
 import { API_TOKEN, API_URL } from "../constants/constants";
 
 export async function fetchProjects() {
-    const result = await fetch(`${API_URL}/projects`, {
-        headers: {
-            Authorization: `Bearer ${API_TOKEN}`,
-        },
-});
-const data = await result.json();
-    return data;
+  const result = await fetch(`${API_URL}/projects`, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
+  const data = await result.json();
+  return data;
 }
--- client/src/queries/fetch-tasks.js
+++ client/src/queries/fetch-tasks.js
@@ -1,11 +1,11 @@
 import { API_TOKEN, API_URL } from "../constants/constants";
 
 export async function fetchTasks() {
-    const result = await fetch(`${API_URL}/tasks?populate=*`, {
-        headers: {
-            Authorization: `Bearer ${API_TOKEN}`,
-        },
-    });
-    const data = await result.json();
-    return data;
+  const result = await fetch(`${API_URL}/tasks?populate=*`, {
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
+  const data = await result.json();
+  return data;
 }
