74c0d3dc9283fe8cce202536dc2a13cb486f7872
smitsleon <leon.smits@student.arteveldehs.be>
2025-06-18 03:05:20 +0200

    Kanban board code cleanup + SPlit in multiple files

--- client/src/components/kanban-board/index.js
+++
@@ -1,6 +0,0 @@
-// Export barrel for kanban board components
-export { KanbanBoard } from "./KanbanBoardRefactored";
-export { default as KanbanCard } from "./KanbanCard";
-export { default as KanbanColumn } from "./KanbanColumn";
-export { default as KanbanFilters } from "./KanbanFilters";
-export * from "./kanbanUtils";
